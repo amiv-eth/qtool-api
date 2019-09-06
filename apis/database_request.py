4a5f26340476c2071d2d3ac99e9703777a68377d
kuehnej-amiv <kuehnej@amiv.ethz.ch>
2019-09-06 19:52:14 +0200

    Rewriting of filter query parser.

--- apis/database_request.py
+++ apis/database_request.py
@@ -4,9 +4,11 @@ from .query_parser import queryParser
 def loadPage(dbClass, dbSchema):
     args = queryParser(dbClass)
     index = args["page"]
-    embeddedQuery = dbClass.query.options(args["embedded"])
-    orderedQuery = embeddedQuery.order_by(args["sort"])
+    (sortBy,primaryDatabase) = args['sort']
+    embeddedQuery = dbClass.query.options(args["embedded"]).outerjoin('user').outerjoin('account')#.outerjoin(primaryDatabase)
+    orderedQuery = embeddedQuery.order_by(sortBy)
     filteredQuery = orderedQuery.filter(args["where"])
+    print(filteredQuery.count())
     paginatedQuery = filteredQuery.offset(25*(index-1)).limit(25)
     dumpedQuery = dbSchema(many=True).dump(paginatedQuery).data
     return dumpedQuery
--- apis/query_parser.py
+++ apis/query_parser.py
@@ -1,8 +1,7 @@
 from flask import request
-
 from flask_restplus import abort
 
-from sqlalchemy import desc, or_, and_
+from sqlalchemy import asc, desc, or_, and_
 from sqlalchemy.orm import relationship, joinedload
 
 from ast import literal_eval
@@ -15,6 +14,7 @@ class QuerySchema(Schema):
     page = fields.Int()
     embedded = fields.Str()
 
+
 def queryParser(dbClass):
     query = request.args.to_dict()
     query = QuerySchema().load(query).data
@@ -25,6 +25,13 @@ def queryParser(dbClass):
     if 'embedded' in query:
         query['embedded'] = embeddingParser(dbClass, query['embedded'])
 
+    if 'where' in query:
+        try:
+            whereDict = literal_eval(query['where'])
+            whereDict = dict(whereDict)
+        except:
+            abort(400, 'Invalid syntax for query parameter where, please pass a valid dicitionary.')
+        query['where'] = filterParser(whereDict,dbClass)
 
     parameters = {
         "where": True,
@@ -32,12 +39,7 @@ def queryParser(dbClass):
         "page": 1,
         "embedded": ()
         }
-
-
-
-
     parameters.update(query)
-
     return parameters
 
 def decomposeNestedOperator(dbClass, opString):
@@ -47,11 +49,13 @@ def decomposeNestedOperator(dbClass, opString):
         [relation, attribute, operator] = parameters
         try:
             dbClass = getattr(dbClass, relation).property.entity.class_
+            relatedDatabase = dbClass
         except:
             errorMessage = '[Query Error]: ' + dbClass.__tablename__ + ' has no related database called '+ relation + '.'
             abort(400, errorMessage)    
     elif numberOfParameters == 2:
         [attribute, operator] = parameters
+        relatedDatabase = None
     else: 
         errorMessage = '[Query Error]: Invalid operation: ' + opString + ', the following syntax is expected: (embeddedResource.)attribute.operator.'
         abort(400, errorMessage)
@@ -60,56 +64,20 @@ def decomposeNestedOperator(dbClass, opString):
     except: 
         errorMessage = '[Query Error]: ' + dbClass.__tablename__ + ' has no attribute called '+ attribute + '.'
         abort(400, errorMessage)
-    return (attributeInstance,operator)
+    return (relatedDatabase,attributeInstance,operator)
 
 def sortParser(dbClass, sortingParameter):
     #ToDo: Can not sort by an unloaded related database!
-    (attribute, operator) = decomposeNestedOperator(dbClass, sortingParameter)
+    (relatedDatabase, attribute, operator) = decomposeNestedOperator(dbClass, sortingParameter)
     if operator == 'asc':
-        return attribute
+        return (asc(attribute),relatedDatabase)
     elif operator == 'desc':
-        return desc(attribute)
+        return (desc(attribute),relatedDatabase)
     else:
         errorMessage = '[Query Error]: ' + operator + ' is no valid sorting order. Use asc or desc.'
         abort(400, errorMessage)
 
- 
-"""
-def queryParser(dbClass = None):
-    arguments = request.args.to_dict()
-    schema = QuerySchema()
-    query = schema.load(arguments)[0]
-    args = {}
-    embedding = None
-    if 'page' in query:
-        args['page'] = query['page']
-
-    if 'embedded' in query:
-        try:
-            embeddingDict = literal_eval(query['embedded'])
-        except:
-            abort(400, 'Invalid syntax for query parameter embedded, please pass a valid dicitionary.')
-        if not (type(embeddingDict) is dict):
-            abort(400, 'Invalid syntax for query parameter embedded, please pass a valid dicitionary.')
-        embedding = embeddingParser(embeddingDict, dbClass)
-        args['embedded'] = embedding 
-
-    if 'where' in query:
-        try:
-            whereDict = literal_eval(query['where'])
-        except:
-            abort(400, 'Invalid syntax for query parameter where, please pass a valid dicitionary.')
-        if not (type(whereDict) is dict):
-            abort(400, 'Invalid syntax for query parameter where, please pass a valid dicitionary.')
-        args['where'] = filterParser(whereDict,dbClass, embedding)
-
-    if 'sort' in query:
-        args['sort'] = sortingParser(query['sort'],dbClass, embedding)
-
-    return args
-
-"""
-def filterParser(whereStatement,dbClass, embedding):
+def filterParser(whereStatement,dbClass):
     if len(whereStatement) > 1:
         abort(400, 'Invalid syntax for query parameter where, wrong number of arguments!')
     
@@ -120,7 +88,7 @@ def filterParser(whereStatement,dbClass, embedding):
             abort(400, 'Invalid syntax for query parameter where. An and statement requires a list of dicitionaries!')
         filterCondition = True
         for statement in filterList:
-            filterCondition = and_(filterCondition, filterParser(statement, dbClass, embedding))
+            filterCondition = and_(filterCondition, filterParser(statement, dbClass))
 
     elif 'or' == key:
         filterList = whereStatement[key]
@@ -128,50 +96,33 @@ def filterParser(whereStatement,dbClass, embedding):
             abort(400, 'Invalid syntax for query parameter where. An or statement requires a list of dicitionaries!')
         filterCondition = False
         for statement in filterList:
-            filterCondition = or_(filterCondition, filterParser(statement, dbClass, embedding))
+            filterCondition = or_(filterCondition, filterParser(statement, dbClass))
         # Ensure consistent results, when receiving an empty or statement.
         if len(filterList) == 0:
             filterCondition = True
 
     else:
-        filterCondition = createFilterCondition(key,whereStatement[key], dbClass, embedding)
+        filterCondition = createFilterCondition(key,whereStatement[key], dbClass)
             
     return filterCondition
 
-def createFilterCondition(condition, val, dbClass, embedding):
-    parameters = condition.split('.')
-    embeddedKey = None
-    if len(parameters) == 1:
-        abort(400, 'Invalid syntax for query parameter where, operator missing!')
-    elif len(parameters) == 2:
-        [attr, op] = parameters
-    elif len(parameters) == 3:
-        [embeddedKey, attr, op] = parameters
-    else:
-        abort(400, 'Invalid syntax for query parameter where, to many arguments given!')
-    if embeddedKey:
-        if embeddedKey in embedding:
-            dbClass = embedding[embeddedKey]().databaseName
-        else:
-            abort(400, 'Invalid syntax for query parameter where, invalid key for embedded data given! The key might be correct, but the data not embedded.')
+def createFilterCondition(condition, val, dbClass):
+    (secondaryDatabase, attr, op) = decomposeNestedOperator(dbClass, condition)
     # ToDo: schema load for input validation
     if op == "eq":
-        condition = getattr(dbClass, attr) == val
+        condition = attr == val
     elif op == 'neq':
-        condition = getattr(dbClass, attr) != val
+        condition = attr != val
     elif op == 'lte':
-        condition = getattr(dbClass, attr) <= val
+        condition = attr <= val
     elif op == 'gte':
-        condition = getattr(dbClass, attr) >= val
+        condition = attr >= val
     elif op == 'in':
-        condition = getattr(dbClass, attr)
+        condition = attr
         condition = condition.contains(val)
     else:
         abort(400, 'Invalid syntax for query parameter where, ' + op + ' is not a valid operator!')
-
     return condition
-
-
 
 def embeddingParser(dbClass, embedded):
     try:
